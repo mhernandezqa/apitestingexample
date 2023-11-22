64ae94dfbabb3236a7f29bdc5bded8312b540ce6
mahernandezmartinez@teco.com.ar <ladriver61@gmail.com>
2023-11-22 18:47:51 -0300

    Change testdata

--- data/test_data.py
+++ data/test_data.py
@@ -5,13 +5,13 @@ class TestData:
     base_url = "https://restful-booker.herokuapp.com/booking/"
 
     booking_data = {
-        "firstname" : "Miguel",
-        "lastname" : "Hernandez",
-        "totalprice" : 5000,
+        "firstname" : "John",
+        "lastname" : "Doe",
+        "totalprice" : 500,
         "depositpaid" : True,
         "bookingdates" : {
-            "checkin" : "2023-12-15",
-            "checkout" : "2023-12-25"
+            "checkin" : "2024-12-15",
+            "checkout" : "2024-12-25"
         },
         "additionalneeds" : "Breakfast"
         }
